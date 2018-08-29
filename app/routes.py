84bb9885ecf8b25ec94603fcbdf9e951a73bf417
DmitrySevostianov <dmitry.sevostianov.mephi@yandex.ru>
2018-08-29 12:31:53 +0300

    product _list (index) and _detail temlates added, some simle experiments with lists and dictionaries

--- app/routes.py
+++ app/routes.py
@@ -4,45 +4,69 @@ from flask import render_template
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'дорогой гость'}
-    products = [
+products = [
         {
+            'id': '1',
             'item': {'itemname': 'RaspberryPi'},
             'body': 'single board computer',
             'price': '10'
         },
         {
+            'id': '2',
             'item': {'itemname': 'OrangePi'},
             'body': 'single board computer',
             'price': '8'
 
         }, 
         {
+            'id': '3',
             'item': {'itemname': 'BeagleBone'},
             'body': 'single board computer',
             'price': '12'
         }
     ]
-    return render_template('index.html', title='Home',user=user, products=products)
 
-@app.route('/RaspberryPi')
-def RaspberryPi():
-    return 'RaspberryPi,,.'
+products_dict = {
+        '1':{
+            'id': '1',
+            'item': {'itemname': 'RaspberryPi'},
+            'body': 'single board computer',
+            'price': '10'
+        },
+        '2':{
+            'id': '2',
+            'item': {'itemname': 'OrangePi'},
+            'body': 'single board computer',
+            'price': '8'
 
-@app.route('/OrangePi')
-def OrangePi():
-    return '///OrangePi'
+        }, 
+        '3':{
+            'id': '3',
+            'item': {'itemname': 'BeagleBone'},
+            'body': 'single board computer',
+            'price': '12'
+        }
+    }
 
-@app.route('/BeagleBone')
-def BeagleBone():
-    return 'BeagleBone...'
 
-@app.route('/explore/<product_name>')
-def explore_product(product_name):
-    
-    return product_name
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home', products=products)
+
+
+@app.route('/explore/<product_id>')
+def explore_product(product_id):
+    current_product = {}
+    for product in products:
+        print (product['id'])
+        if product['id'] == product_id:
+            current_product = product
+            
+    print(products_dict[product_id])
+    #print('Requsted product: ',current_product)
+
+    return render_template('detail.html', title='Detail', product=current_product)
+
 
 
